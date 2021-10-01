022e9bf8ccd05874755aa750d7fc989b3f69558c
Hannes Kristján Hannesson <hanneskr4@gmail.com>
2021-10-01 10:01:36 +0000

    Added recursive solution to fibonacci in Assignment 11

--- assignments/lists_and_tuples/fibonacci.py
+++ assignments/lists_and_tuples/fibonacci.py
@@ -9,5 +9,15 @@ def fibo_tuple(n):
         a_n = sum(a)
         a = (a[1], a_n)
 
+# Recursive solution:
+# def fibo_tuple(n, a=(0, 1)):
+#     if n == 0:
+#         return
+#     else:
+#         print(a[1], end=" ")
+#         a_n = sum(a)
+#         a = (a[1], a_n)
+#         fibo_tuple(n-1, a)
+
 if __name__ == "__main__":
     main()
