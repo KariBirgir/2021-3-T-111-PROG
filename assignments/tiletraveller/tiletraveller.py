b0818618d03e61f87e1f95b222f2899b4493028c
POA <peturoa1@gmail.com>
2021-10-06 15:48:17 +0000

    reorder functions in accordance with the step-down-rule

--- assignments/tiletraveller/tiletraveller.py
+++ assignments/tiletraveller/tiletraveller.py
@@ -17,39 +17,6 @@ def main():
         victory, col, row = play_one_move(col, row, valid_directions)
     print("Victory!")
 
-
-def move(direction, col, row):
-    ''' Returns updated col, row given the direction '''
-    if direction == NORTH:
-        row += 1
-    elif direction == SOUTH:
-        row -= 1
-    elif direction == EAST:
-        col += 1
-    elif direction == WEST:
-        col -= 1
-    return(col, row)    
-
-def is_victory(col, row):
-    ''' Return true if player is in the victory cell '''
-    return col == 3 and row == 1 # (3,1)
-
-def print_directions(directions_str):
-    print("You can travel: ", end='')
-    first = True
-    for ch in directions_str:
-        if not first:
-            print(" or ", end='')
-        if ch == NORTH:
-            print("(N)orth", end='')
-        elif ch == EAST:
-            print("(E)ast", end='')
-        elif ch == SOUTH:
-            print("(S)outh", end='')
-        elif ch == WEST:
-            print("(W)est", end='')
-        first = False
-    print(".")
         
 def find_directions(col, row):
     ''' Returns valid directions as a string given the supplied location '''
@@ -71,6 +38,25 @@ def find_directions(col, row):
         valid_directions = SOUTH+WEST
     return valid_directions
 
+
+def print_directions(directions_str):
+    print("You can travel: ", end='')
+    first = True
+    for ch in directions_str:
+        if not first:
+            print(" or ", end='')
+        if ch == NORTH:
+            print("(N)orth", end='')
+        elif ch == EAST:
+            print("(E)ast", end='')
+        elif ch == SOUTH:
+            print("(S)outh", end='')
+        elif ch == WEST:
+            print("(W)est", end='')
+        first = False
+    print(".")
+
+
 def play_one_move(col, row, valid_directions):
     ''' Plays one move of the game
         Return if victory has been obtained and updated col,row '''
@@ -84,6 +70,24 @@ def play_one_move(col, row, valid_directions):
         col, row = move(direction, col, row)
         victory = is_victory(col, row)
     return victory, col, row
+
+
+def move(direction, col, row):
+    ''' Returns updated col, row given the direction '''
+    if direction == NORTH:
+        row += 1
+    elif direction == SOUTH:
+        row -= 1
+    elif direction == EAST:
+        col += 1
+    elif direction == WEST:
+        col -= 1
+    return(col, row)
+
+
+def is_victory(col, row):
+    ''' Return true if player is in the victory cell '''
+    return col == 3 and row == 1 # (3,1)
 
 
 if __name__ == "__main__":
